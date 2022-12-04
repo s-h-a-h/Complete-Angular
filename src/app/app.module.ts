240fcb6c824ba6f73219d99b610cc63b441cf19a
Shah <workwithme.shah@gmail.com>
2022-12-04 22:59:25 +0530

    item-a comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemAComponent } from './item-a/item-a.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemAComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/item-a/item-a.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-a',
+  templateUrl: './item-a.component.html',
+  styleUrls: ['./item-a.component.css']
+})
+export class ItemAComponent {
+
+}
