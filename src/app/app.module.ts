af68ae533cfec08cca09e578816b7f3eb3ebe571
Shah <workwithme.shah@gmail.com>
2022-12-04 23:02:33 +0530

    item-b comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemAComponent } from './item-a/item-a.component';
+import { ItemBComponent } from './item-b/item-b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemAComponent
+    ItemAComponent,
+    ItemBComponent
   ],
   imports: [
     BrowserModule,
