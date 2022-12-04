17eb5d31d979e41c8de59e6067fc8a7bfd73c48d
Shah <workwithme.shah@gmail.com>
2022-12-04 23:03:32 +0530

    item-b comp added

---
+++ src/app/item-b/item-b.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemBComponent } from './item-b.component';
+
+describe('ItemBComponent', () => {
+  let component: ItemBComponent;
+  let fixture: ComponentFixture<ItemBComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ItemBComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ItemBComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
