cde114978d0169a7ae31f8923944745c4a0709b3
Shah <mraza@allegion.com>
2022-12-05 22:39:22 +0530

    event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,52 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  action = "MAKE ME SMILE";
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
+  serverStatus = "Server is not yet created";
+  onCreateServer = () => {
+    this.serverStatus = "Server Created 😊"
+  }
+  emojiStatus = "";
+
+  onClickStatusSmile = () => {
+    this.emojiStatus = "😊";
+    this.action = "Smiling...";
+  }
+  onClickStatusSad = () => {
+    this.emojiStatus = "😞";
+    this.action = "Sad...";
+
+  }
+  onClickStatusCry = () => {
+    this.emojiStatus = "😢";
+    this.action = "Crying...";
+
+  }
+  onClickStatusLaugh = () => {
+    this.emojiStatus = "😂";
+    this.action = "Laughing..";
+
+  }
+  onClickStatusWink = () => {
+    this.emojiStatus = "😉";
+    this.action = "Winking..";
+
+  }
+  onClickStatusHeart = () => {
+    this.emojiStatus = "♥";
+    this.action = "Love love..";
+
+  }
+  onClickStatusLike = () => {
+    this.emojiStatus = "👍";
+    this.action = "Liking..";
+
+  }
+  
 }
