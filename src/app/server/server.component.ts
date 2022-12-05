1a51620ac9d2aa6734aa059d8df61386a4d50143
Shah <workwithme.shah@gmail.com>
2022-12-05 23:07:56 +0530

    toggle status - event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,14 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-  name1 = "nginx";
-  name2= "Apache";
-  name3 = "cloudflare";
   isActive = (serverName: string): boolean => {
     if (serverName === "nginx" || serverName === "cloudflare") {
       return false;
     } else {
       return true;
+    }
+  }
+  name1 = "nginx";
+  name2 = "Apache";
+  name3 = "cloudflare";
+  status1 = this.isActive(this.name1);
+  status2 = this.isActive(this.name2);
+  status3 = this.isActive(this.name3);
+  toggleStatus = () => {
+    if (this.status1 === true) {
+      this.status1 = false;
+    } else {
+      this.status1 = true;
+    }
+    if (this.status2 === true) {
+      this.status2 = false;
+    } else {
+      this.status2 = true;
+    }
+    if (this.status3 === true) {
+      this.status3 = false;
+    } else {
+      this.status3 = true;
     }
   }
 }
