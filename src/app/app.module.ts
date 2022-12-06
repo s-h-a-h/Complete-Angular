4f288e729796e56dede1d84a0e8c06c65555d4f2
Shah <workwithme.shah@gmail.com>
2022-12-06 19:03:32 +0530

    practise comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { SuccessComponent } from './practise/success/success.component';
+import { WarningComponent } from "./practise/warning/warning.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/practise/success/success.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-success',
+    templateUrl: './success.component.html',
+    styleUrls: ['./success.component.css']
+})
+
+export class SuccessComponent {
+    success = false;
+    onClick = () => {
+        this.success = true;
+    }
+}
