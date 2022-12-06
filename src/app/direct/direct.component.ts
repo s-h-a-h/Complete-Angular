2958ad528174c04bf548f0889f0a7ab32d7a06bf
Shah <workwithme.shah@gmail.com>
2022-12-06 22:07:10 +0530

    Styling elements dynamically using ngStyle

--- src/app/direct/direct.component.ts
+++ src/app/direct/direct.component.ts
@@ -6,11 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./direct.component.css']
 })
 export class DirectComponent {
+  serverStatus = "offline";
+
   serverName = "";
   serverCreated = false;
+
   onClick = () => {
     if (this.serverName.length > 0) {
       this.serverCreated = true;
     }
+  }
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? "online" : "ofline";
+  }
+
+  getServerStatus = () => {
+    return this.serverStatus;
+  }
+
+  getColor = () => {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
