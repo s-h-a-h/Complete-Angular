7ff94de8cf6cdec1a05692121d7e127278d3109d
Shah <workwithme.shah@gmail.com>
2022-12-06 21:29:11 +0530

    folder rename

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { ServerComponent } from './databinding/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
 import { SuccessComponent } from './practise/success/success.component';
